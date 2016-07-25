386111bc32150f85deb5f515b8dfb2e019b629b0
Jacob Friedmann <jacobdfriedmann@gmail.com>
2016-07-25 08:23:53 -0400

    Init oauth

--- js/app.js
+++ js/app.js
@@ -1,113 +1,166 @@
-$(document).ready(function() {
+var apiKey = 'AIzaSyDRTXMhmEXbSGuIAzWFwpP4eIQTIyNSnSc';
+var clientId = '563107140751-a15af7rpl8nu88i5srh1vgcb8r54u6uh.apps.googleusercontent.com';
+var scopes = 'profile';
+var auth2;
 
-  var model = {
-    loggedIn: false,
-    userId: undefined,
-    username: undefined,
-    categories: undefined,
-    articles: undefined
-  };
+function handleGoogleClientLoad() {
+  gapi.load('client:auth2', function() {
+    gapi.client.setApiKey(apiKey);
+    gapi.auth2.init({
+        client_id: clientId,
+        scope: scopes
+    }).then(function () {
+      auth2 = gapi.auth2.getAuthInstance();
+      auth2.isSignedIn.listen(handleGoogleLoginComplete);
+      handleGoogleLoginComplete(auth2.isSignedIn.get())
+    });
+  });
+}
 
-  function renderLogin() {
-    var source = $('#login-template').html();
-    var template = Handlebars.compile(source);
-    var renderedTemplate = template(model);
 
-    $('#loginContainer').html(renderedTemplate);
-  }
+var model = {
+  loggedIn: false,
+  userId: undefined,
+  username: undefined,
+  categories: undefined,
+  articles: undefined
+};
 
-  function renderCategories() {
-    var source = $('#category-template').html();
-    var template = Handlebars.compile(source);
-    var renderedTemplate = template(model);
+function renderLogin() {
+  var source = $('#login-template').html();
+  var template = Handlebars.compile(source);
+  var renderedTemplate = template(model);
 
-    $('#categories').html(renderedTemplate);
-  }
+  $('#loginContainer').html(renderedTemplate);
+}
 
-  function renderArticles() {
-    var source = $('#article-template').html();
-    var template = Handlebars.compile(source);
-    var renderedTemplate = template(model);
+function renderCategories() {
+  var source = $('#category-template').html();
+  var template = Handlebars.compile(source);
+  var renderedTemplate = template(model);
 
-    $('#main').html(renderedTemplate);
-  }
+  $('#categories').html(renderedTemplate);
+}
 
-  function setup() {
+function renderArticles() {
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+  var renderedTemplate = template(model);
+
+  $('#main').html(renderedTemplate);
+}
+
+function setup() {
+  renderLogin();
+
+  window.handleGoogleClientLoad = handleGoogleClientLoad;
+
+  $('#loginContainer').on('click', '#loginGoogle', handleGoogleLogin);
+  $('#loginContainer').on('click', '#logoutGoogle', handleGoogleLogout);
+  $('#categories').on('click', '.label', handleToggleCategory);
+  $('#main').on('click', 'article', handleToggleBookmark);
+}
+
+function handleGoogleLogin() {
+  auth2.signIn();
+}
+
+function handleGoogleLogout() {
+  auth2.signOut();
+}
+
+function handleGoogleLoginComplete(isSignedIn) {
+  if (isSignedIn) {
+    gapi.client.load('people', 'v1', function() {
+      var request = gapi.client.people.people.get({
+        resourceName: 'people/me'
+      });
+      request.execute(function(resp) {
+        var googleId = resp.metadata.sources[0].id;
+        var googleName = resp.names[0].displayName;
+        handleLogin(googleId, googleName);
+      });
+    });
+  } else {
+    model = {
+      loggedIn: false,
+      userId: undefined,
+      username: undefined,
+      categories: undefined,
+      articles: undefined
+    };
     renderLogin();
-
-    $('#loginContainer').on('click', '#loginSubmit', handleLogin);
-    $('#categories').on('click', '.label', handleToggleCategory);
-    $('#main').on('click', 'article', handleToggleBookmark);
+    renderCategories();
+    renderArticles();
   }
+}
 
-  function handleLogin() {
-    var username = $('#username').val();
-    $.post('/login', { username: username }, function (data) {
-      model.loggedIn = true;
-      model.userId = data.userId;
-      model.username = username;
+function handleLogin(googleId, username) {
+  $.post('/login', { username: googleId }, function (data) {
+    model.loggedIn = true;
+    model.userId = data.userId;
+    model.username = username;
 
+    loadCategories();
+    loadArticles();
+    renderLogin();
+  });
+}
+
+function loadCategories() {
+  $.get('/categories?userId=' + model.userId, function (data) {
+    model.categories = data;
+
+    renderCategories();
+  });
+}
+
+function loadArticles() {
+  $.get('/feed?userId=' + model.userId, function (data) {
+    model.articles = data;
+
+    renderArticles();
+  });
+}
+
+function handleToggleCategory() {
+  var category = $(this).text();
+  if ($(this).hasClass('label-default')) {
+    $.post('/interests/' + category + '?userId=' + model.userId, function (data) {
       loadCategories();
       loadArticles();
-      renderLogin();
     });
-  }
-
-  function loadCategories() {
-    $.get('/categories?userId=' + model.userId, function (data) {
-      model.categories = data;
-
-      renderCategories();
-    });
-  }
-
-  function loadArticles() {
-    $.get('/feed?userId=' + model.userId, function (data) {
-      model.articles = data;
-
-      renderArticles();
-    });
-  }
-
-  function handleToggleCategory() {
-    var category = $(this).text();
-    if ($(this).hasClass('label-default')) {
-      $.post('/interests/' + category + '?userId=' + model.userId, function (data) {
+  } else {
+    $.ajax({
+      type: 'DELETE',
+      url: '/interests/' + category + '?userId=' + model.userId,
+      success: function (data) {
         loadCategories();
         loadArticles();
-      });
-    } else {
-      $.ajax({
-        type: 'DELETE',
-        url: '/interests/' + category + '?userId=' + model.userId,
-        success: function (data) {
-          loadCategories();
-          loadArticles();
-        }
-      });
-    }
+      }
+    });
   }
+}
 
-  function handleToggleBookmark() {
-    var articleIndex = $(this).index();
-    var article = model.articles[articleIndex];
-    var articleId = article.id;
-    var bookmarked = article.bookmarked;
+function handleToggleBookmark() {
+  var articleIndex = $(this).index();
+  var article = model.articles[articleIndex];
+  var articleId = article.id;
+  var bookmarked = article.bookmarked;
 
-    if (!bookmarked) {
-      $.post('/bookmarks/' + articleId + '?userId=' + model.userId, function (data) {
+  if (!bookmarked) {
+    $.post('/bookmarks/' + articleId + '?userId=' + model.userId, function (data) {
+      loadArticles();
+    });
+  } else {
+    $.ajax({
+      type: 'DELETE',
+      url: '/bookmarks/' + articleId + '?userId=' + model.userId,
+      success: function (data) {
         loadArticles();
-      });
-    } else {
-      $.ajax({
-        type: 'DELETE',
-        url: '/bookmarks/' + articleId + '?userId=' + model.userId,
-        success: function (data) {
-          loadArticles();
-        }
-      });
-    }
+      }
+    });
   }
+}
 
-  setup();
-});
+$(document).ready(setup);
