c4aa2d5d41cc02b63e13c65e55dcbb2235ea2a96
Jacob Friedmann <jacobdfriedmann@gmail.com>
2016-07-31 17:31:30 -0400

    Add hint 2

--- js/app.js
+++ js/app.js
@@ -1,43 +1,11 @@
 // Model
 
 var model = {
-  loggedIn: true,
-  userId: '1234',
-  username: 'jacob',
-  categories: [
-    {
-      name: 'science',
-      selected: false
-    },
-    {
-      name: 'politics',
-      selected: true
-    }
-  ],
-  articles: [
-    {
-      "id": -1738676198,
-      "title": "Review: In ‘Jason Bourne,’ a Midlife Crisis for a Harried Former Assassin",
-      "url": "http://www.nytimes.com/2016/07/29/movies/jason-bourne-matt-damon-review.html",
-      "imageUrl": "https://static01.nyt.com/images/2016/07/29/arts/29JASONBOURNE1SUB/29JASONBOURNE1SUB-thumbStandard.jpg",
-      "description": "Matt Damon returns in a film in which geopolitics and technology are scaffolding for what is essentially a story about human resources challenges in a large bureaucracy.",
-      "category": "movies",
-      "date": "2016-07-29T04:00:00.000Z",
-      "source": "New York Times",
-      "bookmarked": false
-    },
-    {
-      "id": -2083806667,
-      "title": "Google Silences Doubters With Blockbuster Quarter",
-      "url": "http://www.nytimes.com/2016/07/29/technology/alphabet-google-earnings.html",
-      "imageUrl": "https://static01.nyt.com/images/2016/07/29/business/29ALPHABET-1469744816857/29ALPHABET-1469744816857-thumbStandard.jpg",
-      "description": "It was a major recovery from a lukewarm previous quarter, proving that the internet behemoth still has room and energy to grow.",
-      "category": "tech",
-      "date": "2016-07-29T04:00:00.000Z",
-      "source": "New York Times",
-      "bookmarked": true
-    }
-  ]
+  loggedIn: false,
+  userId: '',
+  username: '',
+  categories: [],
+  articles: []
 };
 
 // View
@@ -79,8 +47,40 @@ function setup() {
   renderLogin();
 
   // Event Listeners
-  renderArticles();
-  renderCategories();
+  $('#loginContainer').on('click', '#loginSubmit', login);
+  $('#categories').on('click', 'span', toggleInterest);
+}
+
+function login() {
+  var enteredUsername = $('#username').val();
+
+  $.post('/login', {username: enteredUsername}, function (data) {
+    model.username = enteredUsername;
+    model.userId = data.userId;
+    model.loggedIn = true;
+
+    // Load categories and feed for this user, rerender
+  });
+}
+
+function toggleInterest() {
+  var isUserInterest = $(this).hasClass('label-primary');
+  var interestName = $(this).text();
+  var url = '/interests/' + interestName + '?userId=' + model.userId;
+
+  if (isUserInterest) {
+    $.ajax({
+      type: 'DELETE',
+      url: url,
+      success: function() {
+        // update categories and feed, rerender
+      }
+    });
+  } else {
+    $.post(url, function() {
+      // update categories and feed, rerender
+    });
+  }
 }
 
 $(document).ready(setup);
